94952950f8cb503b459fd6c4748f36e413d605f6
Andrew Haigh <andrew.haigh@cellsoftware.co.uk>
2021-02-08 17:54:05 +0000

    Add support for passing the API Key as the username (#69)
    * Add support for passing the API Key as the username
    
    * PR Comment. Now uses StringUtils isNotEmpty()
    
    * PR Comment. replace tab with 4 spaces

--- src/main/java/com/testdroid/jenkins/utils/TestdroidApiUtil.java
+++ src/main/java/com/testdroid/jenkins/utils/TestdroidApiUtil.java
@@ -2,6 +2,7 @@ package com.testdroid.jenkins.utils;
 
 import com.testdroid.api.APIClient;
 import com.testdroid.api.DefaultAPIClient;
+import com.testdroid.api.APIKeyClient;
 import com.testdroid.jenkins.TestdroidCloudSettings;
 import com.testdroid.jenkins.remotesupport.MachineIndependentTask;
 import hudson.Extension;
@@ -37,28 +38,55 @@ public class TestdroidApiUtil {
     public static ApiClientAdapter createNewApiClient(MachineIndependentTask machineIndependentTask) {
         APIClient apiClient;
 
-        if (machineIndependentTask.isProxy) {
-            HttpHost proxy = machineIndependentTask.proxyPort != null ?
-                    new HttpHost(machineIndependentTask.proxyHost,
-                            machineIndependentTask.proxyPort, "http") :
-                    new HttpHost(machineIndependentTask.proxyHost);
+        if (StringUtils.isNotEmpty(machineIndependentTask.password)) {
+            // If the password is set, try username and password authentication
+            if (machineIndependentTask.isProxy) {
+                HttpHost proxy = machineIndependentTask.proxyPort != null ?
+                        new HttpHost(machineIndependentTask.proxyHost,
+                                machineIndependentTask.proxyPort, "http") :
+                        new HttpHost(machineIndependentTask.proxyHost);
 
-            apiClient = StringUtils.isBlank(machineIndependentTask.proxyUser) ?
-                    new DefaultAPIClient(machineIndependentTask.cloudUrl,
-                            machineIndependentTask.user,
-                            machineIndependentTask.password, proxy,
-                            machineIndependentTask.noCheckCertificate) :
-                    new DefaultAPIClient(machineIndependentTask.cloudUrl,
-                            machineIndependentTask.user,
-                            machineIndependentTask.password, proxy,
-                            machineIndependentTask.proxyUser,
-                            machineIndependentTask.proxyPassword,
-                            machineIndependentTask.noCheckCertificate);
+                apiClient = StringUtils.isBlank(machineIndependentTask.proxyUser) ?
+                        new DefaultAPIClient(machineIndependentTask.cloudUrl,
+                                machineIndependentTask.user,
+                                machineIndependentTask.password, proxy,
+                                machineIndependentTask.noCheckCertificate) :
+                        new DefaultAPIClient(machineIndependentTask.cloudUrl,
+                                machineIndependentTask.user,
+                                machineIndependentTask.password, proxy,
+                                machineIndependentTask.proxyUser,
+                                machineIndependentTask.proxyPassword,
+                                machineIndependentTask.noCheckCertificate);
+            } else {
+                apiClient = new DefaultAPIClient(machineIndependentTask.cloudUrl,
+                        machineIndependentTask.user,
+                        machineIndependentTask.password,
+                        machineIndependentTask.noCheckCertificate);
+            } 
         } else {
-            apiClient = new DefaultAPIClient(machineIndependentTask.cloudUrl,
-                    machineIndependentTask.user,
-                    machineIndependentTask.password,
-                    machineIndependentTask.noCheckCertificate);
+            // Try the username as an apikey
+            if (machineIndependentTask.isProxy) {
+                HttpHost proxy = machineIndependentTask.proxyPort != null ?
+                        new HttpHost(machineIndependentTask.proxyHost,
+                                machineIndependentTask.proxyPort, "http") :
+                        new HttpHost(machineIndependentTask.proxyHost);
+
+                apiClient = StringUtils.isBlank(machineIndependentTask.proxyUser) ?
+                        new APIKeyClient(machineIndependentTask.cloudUrl,
+                                machineIndependentTask.user,
+                                proxy,
+                                machineIndependentTask.noCheckCertificate) :
+                        new APIKeyClient(machineIndependentTask.cloudUrl,
+                                machineIndependentTask.user,
+                                proxy,
+                                machineIndependentTask.proxyUser,
+                                machineIndependentTask.proxyPassword,
+                                machineIndependentTask.noCheckCertificate);
+            } else {
+                apiClient = new APIKeyClient(machineIndependentTask.cloudUrl,
+                        machineIndependentTask.user,
+                        machineIndependentTask.noCheckCertificate);
+            }
         }
         return new ApiClientAdapter(apiClient);
     }
@@ -70,17 +98,34 @@ public class TestdroidApiUtil {
         boolean dontCheckCert = settings.getNoCheckCertificate();
         APIClient apiClient;
 
-        if (settings.getIsProxy()) {
-            HttpHost proxy = settings.getProxyPort() != null ?
-                    new HttpHost(settings.getProxyHost(), settings.getProxyPort(), "http") :
-                    new HttpHost(settings.getProxyHost());
+        if (StringUtils.isNotEmpty(password)) {
+            // If the password is set, try username and password authentication
+            if (settings.getIsProxy()) {
+                HttpHost proxy = settings.getProxyPort() != null ?
+                        new HttpHost(settings.getProxyHost(), settings.getProxyPort(), "http") :
+                        new HttpHost(settings.getProxyHost());
 
-            apiClient = StringUtils.isBlank(settings.getProxyUser()) ?
-                    new DefaultAPIClient(cloudURL, email, password, proxy, dontCheckCert) :
-                    new DefaultAPIClient(cloudURL, email, password, proxy,
-                            settings.getProxyUser(), settings.getProxyPassword(), dontCheckCert);
+                apiClient = StringUtils.isBlank(settings.getProxyUser()) ?
+                        new DefaultAPIClient(cloudURL, email, password, proxy, dontCheckCert) :
+                        new DefaultAPIClient(cloudURL, email, password, proxy,
+                                settings.getProxyUser(), settings.getProxyPassword(), dontCheckCert);
+            } else {
+                apiClient = new DefaultAPIClient(cloudURL, email, password, dontCheckCert);
+            }
         } else {
-            apiClient = new DefaultAPIClient(cloudURL, email, password, dontCheckCert);
+            // Try the username as an apikey
+            if (settings.getIsProxy()) {
+                HttpHost proxy = settings.getProxyPort() != null ?
+                        new HttpHost(settings.getProxyHost(), settings.getProxyPort(), "http") :
+                        new HttpHost(settings.getProxyHost());
+
+                apiClient = StringUtils.isBlank(settings.getProxyUser()) ?
+                        new APIKeyClient(cloudURL, email, dontCheckCert) :
+                        new APIKeyClient(cloudURL, email, proxy,
+                                settings.getProxyUser(), settings.getProxyPassword(), dontCheckCert);
+            } else {
+                apiClient = new APIKeyClient(cloudURL, email, dontCheckCert);
+            }
         }
         return new ApiClientAdapter(apiClient);
     }
